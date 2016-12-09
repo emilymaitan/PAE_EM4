c71dc26272ef59b69bf91594362d794b5392fb84
Mai <emily.maitan@gmail.com>
2016-12-09 14:05:10 +0100

    add documentation for homepage controller

--- src/Controller/HomepageController.php
+++ src/Controller/HomepageController.php
@@ -3,7 +3,13 @@
 namespace emilymaitan\PAE_EM4\Controller;
 
 class HomepageController extends AbstractController {
+    /**
+     * Fills the template homepage.twig with the necessary information
+     * @return array All status variables for rendering the homepage.
+     */
     public function homepage() {
+
+
         return [
             'status' => 'running',
         ];
