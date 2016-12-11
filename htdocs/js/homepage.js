88d0a47ad7ba567c22a2962bc256b00ac04781e6
Mai <emily.maitan@gmail.com>
2016-12-11 15:37:10 +0100

    add linking to projects to homepage

--- htdocs/js/homepage.js
+++ htdocs/js/homepage.js
@@ -2,11 +2,15 @@
  * Created by Emily on 12/9/2016.
  */
 
-$(function() {
+$ (document).ready(function(){
     $("#datepicker").datepicker({
         onSelect: function(dateText,inst) {
             document.getElementById("filterDate").value = dateText;
             document.getElementById("dateform").submit();
         }
+    });
+
+    $('tr[data-href]').on("click",function() {
+        document.location = $(this).data('href');
     });
 });
