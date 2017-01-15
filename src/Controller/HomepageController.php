c34f7e06e0fff2cb549c598e4c93cf382adaaf14
Mai <emily.maitan@gmail.com>
2017-01-15 16:31:31 +0100

    plug in api status test to home controller

--- src/Controller/HomepageController.php
+++ src/Controller/HomepageController.php
@@ -14,12 +14,20 @@ class HomepageController extends AbstractController {
         $stats_db_entries = 0;
         $stats_db_lastUpdated = new \DateTime();
 
+        switch($this->getApiConnector()->getStatus()) {
+            case 0: $stats_api_status = "ONLINE"; break;
+            case 500: $stats_api_status = "INTERNAL ERROR"; break;
+            case 404:
+            case 503: $stats_api_status = "OFFLINE"; break;
+            default: $stats_api_status = "UNKNOWN";
+        }
+
         // TODO set date to today (for now keep old ones so that we have sth to display)
         $projects_today = $this->getApiConnector()->getProjectByDate("2016/12/11");
 
         return [
             'pagetitle' => "CFM | Homepage",
-            'stats_api_connected' => 'OFFLINE',
+            'stats_api_status' => $stats_api_status,
             'stats_db_entries' => $stats_db_entries,
             'stats_db_lastUpdated' => $stats_db_lastUpdated->format('Y-m-d H:i:s'),
             'projects_today' => $projects_today
